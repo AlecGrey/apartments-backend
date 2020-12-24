ec6bd28e99665c21a2540d8db5be2b887b06df6c
AlecGrey <alecgrey.coding@gmail.com>
2020-12-24 08:10:49 -0800

    added token encode/decode logic to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,46 @@
 class ApplicationController < ActionController::Base
+
+    def encode_token(payload)
+        # generate encrypted token for a user
+        JWT.encode(payload, 'secret')
+    end
+
+    def auth_header
+        # returns a string of 'Bearer token'
+        request.headers['authorization']
+    end
+
+    def decoded_token
+        # decode token if it exists in the headers
+        if auth_header
+            # receive the auth_header, remove 'Bearer ' from the string
+            token = auth_header.split(' ')[1]
+            begin
+                # decode the token
+                JWT.decode(token, 'secret')
+            rescue JWT::DecodeError
+                # catch and handle error
+                nil
+            end
+        end
+    end
+
+    def current_user
+        # checks for a decoded_token, returns the current user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            current_user = User.find(user_id)
+        end
+    end
+
+    def logged_in?
+        # is current user?
+        !!current_user
+    end
+
+    def authorized
+        # checks logged in status, if fails render JSON
+        # use to short-circuit controller actions that require login status
+        render json: {error: "Please log in"}, status: :unauthorized unless logged_in?
+    end
 end
